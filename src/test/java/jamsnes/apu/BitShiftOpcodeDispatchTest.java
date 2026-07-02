4f7bc9a65d8416d4e6c84022502957e621bc867f
ahianf <ahian.fernandez@outlook.com>
2026-07-02 13:29:19 -0400

    test java apu shift rotation opcode dispatch

--- src/test/java/jamsnes/apu/BitShiftOpcodeDispatchTest.java
+++ src/test/java/jamsnes/apu/BitShiftOpcodeDispatchTest.java
@@ -87,6 +87,127 @@ class BitShiftOpcodeDispatchTest {
     }
 
     @Test
+    void executesAslAddressingOpcodes() {
+        SNES snes = init();
+        snes.apu.internalRegisters().pc = 0x200;
+        snes.apu.internalRegisters().x = 0x02;
+        snes.apu._internalWrite(0x0300, 0x40);
+        snes.apu._internalWrite(0x52, 0x81);
+        snes.apu._internalWrite(0x200, 0x0c);
+        writeAbsoluteOperand(snes, 0x201, 0x0300);
+        snes.apu._internalWrite(0x203, 0x1b);
+        snes.apu._internalWrite(0x204, 0x50);
+        snes.apu._internalWrite(0x205, 0x1c);
+
+        assertEquals(5, snes.apu.executeInstruction());
+        assertEquals(0x80, snes.apu._internalRead(0x0300));
+        assertFalse(snes.apu.internalRegisters().c);
+        assertTrue(snes.apu.internalRegisters().n);
+
+        assertEquals(5, snes.apu.executeInstruction());
+        assertEquals(0x02, snes.apu._internalRead(0x52));
+        assertTrue(snes.apu.internalRegisters().c);
+
+        snes.apu.internalRegisters().a = 0x7f;
+        assertEquals(2, snes.apu.executeInstruction());
+        assertEquals(0xfe, snes.apu.internalRegisters().a);
+        assertFalse(snes.apu.internalRegisters().c);
+    }
+
+    @Test
+    void executesRolAddressingOpcodes() {
+        SNES snes = init();
+        snes.apu.internalRegisters().pc = 0x200;
+        snes.apu.internalRegisters().x = 0x02;
+        snes.apu._internalWrite(0x50, 0x80);
+        snes.apu._internalWrite(0x0300, 0x7f);
+        snes.apu._internalWrite(0x0302, 0x01);
+        snes.apu._internalWrite(0x200, 0x2b);
+        snes.apu._internalWrite(0x201, 0x50);
+        snes.apu._internalWrite(0x202, 0x2c);
+        writeAbsoluteOperand(snes, 0x203, 0x0300);
+        snes.apu._internalWrite(0x205, 0x3b);
+        writeAbsoluteOperand(snes, 0x206, 0x0300);
+        snes.apu._internalWrite(0x208, 0x3c);
+
+        assertEquals(4, snes.apu.executeInstruction());
+        assertEquals(0x00, snes.apu._internalRead(0x50));
+        assertTrue(snes.apu.internalRegisters().c);
+
+        assertEquals(5, snes.apu.executeInstruction());
+        assertEquals(0xff, snes.apu._internalRead(0x0300));
+        assertFalse(snes.apu.internalRegisters().c);
+
+        assertEquals(5, snes.apu.executeInstruction());
+        assertEquals(0x02, snes.apu._internalRead(0x0302));
+
+        snes.apu.internalRegisters().a = 0x40;
+        snes.apu.internalRegisters().c = true;
+        assertEquals(2, snes.apu.executeInstruction());
+        assertEquals(0x81, snes.apu.internalRegisters().a);
+    }
+
+    @Test
+    void executesLsrAddressingOpcodes() {
+        SNES snes = init();
+        snes.apu.internalRegisters().pc = 0x200;
+        snes.apu.internalRegisters().x = 0x02;
+        snes.apu._internalWrite(0x50, 0x02);
+        snes.apu._internalWrite(0x0300, 0x01);
+        snes.apu._internalWrite(0x52, 0x80);
+        snes.apu._internalWrite(0x200, 0x4b);
+        snes.apu._internalWrite(0x201, 0x50);
+        snes.apu._internalWrite(0x202, 0x4c);
+        writeAbsoluteOperand(snes, 0x203, 0x0300);
+        snes.apu._internalWrite(0x205, 0x5b);
+        snes.apu._internalWrite(0x206, 0x50);
+
+        assertEquals(4, snes.apu.executeInstruction());
+        assertEquals(0x01, snes.apu._internalRead(0x50));
+        assertFalse(snes.apu.internalRegisters().c);
+        assertTrue(snes.apu.internalRegisters().n);
+
+        assertEquals(5, snes.apu.executeInstruction());
+        assertEquals(0x00, snes.apu._internalRead(0x0300));
+        assertTrue(snes.apu.internalRegisters().c);
+        assertTrue(snes.apu.internalRegisters().z);
+
+        assertEquals(5, snes.apu.executeInstruction());
+        assertEquals(0x40, snes.apu._internalRead(0x52));
+        assertFalse(snes.apu.internalRegisters().c);
+    }
+
+    @Test
+    void executesRorAddressingOpcodes() {
+        SNES snes = init();
+        snes.apu.internalRegisters().pc = 0x200;
+        snes.apu.internalRegisters().x = 0x02;
+        snes.apu._internalWrite(0x50, 0x02);
+        snes.apu._internalWrite(0x0300, 0x01);
+        snes.apu._internalWrite(0x52, 0x80);
+        snes.apu._internalWrite(0x200, 0x6b);
+        snes.apu._internalWrite(0x201, 0x50);
+        snes.apu._internalWrite(0x202, 0x6c);
+        writeAbsoluteOperand(snes, 0x203, 0x0300);
+        snes.apu._internalWrite(0x205, 0x7b);
+        snes.apu._internalWrite(0x206, 0x50);
+
+        assertEquals(4, snes.apu.executeInstruction());
+        assertEquals(0x01, snes.apu._internalRead(0x50));
+        assertFalse(snes.apu.internalRegisters().c);
+        assertTrue(snes.apu.internalRegisters().n);
+
+        assertEquals(5, snes.apu.executeInstruction());
+        assertEquals(0x00, snes.apu._internalRead(0x0300));
+        assertTrue(snes.apu.internalRegisters().c);
+        assertTrue(snes.apu.internalRegisters().z);
+
+        assertEquals(5, snes.apu.executeInstruction());
+        assertEquals(0x41, snes.apu._internalRead(0x52));
+        assertFalse(snes.apu.internalRegisters().c);
+    }
+
+    @Test
     void executesAccumulatorRightShiftOpcodes() {
         SNES snes = init();
         snes.apu.internalRegisters().pc = 0x200;
@@ -110,6 +231,11 @@ class BitShiftOpcodeDispatchTest {
         snes.apu._internalWrite(pc, opcode);
         snes.apu._internalWrite(pc + 1, operand);
         snes.apu._internalWrite(pc + 2, operand >>> 8);
+    }
+
+    private static void writeAbsoluteOperand(SNES snes, int pc, int address) {
+        snes.apu._internalWrite(pc, address);
+        snes.apu._internalWrite(pc + 1, address >>> 8);
     }
 
     private static SNES init() {
