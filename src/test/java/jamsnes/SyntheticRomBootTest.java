3da847ffcf26ad5d68dd3df44bd44dc365645fbf
ahianf <ahian.fernandez@outlook.com>
2026-07-24 11:09:19 -0400

    add synthetic lorom hdma gate

--- src/test/java/jamsnes/SyntheticRomBootTest.java
+++ src/test/java/jamsnes/SyntheticRomBootTest.java
@@ -86,6 +86,25 @@ class SyntheticRomBootTest {
     }
 
     @Test
+    void loadedLoRomRunsHdmaAtHBlankAndPreservesThePriorScanline() throws IOException {
+        FrameBufferRenderer renderer =
+                new FrameBufferRenderer(Background.BUFFER_SIZE, Background.BUFFER_SIZE, 60);
+        SNES snes = new SNES(writeHdmaRom().toString(), renderer);
+
+        for (int updates = 0; renderer.drawScreenCalls() < 2 && updates < 20_000; updates++) {
+            snes.update();
+        }
+
+        assertEquals(2, renderer.drawScreenCalls(), "Synthetic ROM should present its HDMA-updated frame");
+        assertEquals(PPUUtils.cgramColorToRGBA(0x001f), renderer.pixel(0, 0));
+        assertEquals(PPUUtils.cgramColorToRGBA(0x001f), renderer.pixel(1, 0));
+        assertEquals(PPUUtils.cgramColorToRGBA(0x03e0), renderer.pixel(2, 0));
+        assertEquals(0x8103, snes.cpu.dmaChannels()[0].getTableAddress());
+        assertEquals(0x8114, snes.cpu.dmaChannels()[1].getTableAddress());
+        assertEquals(0x8051, snes.cpu.registers().pc);
+    }
+
+    @Test
     void loadedLoRomReadsAutomaticJoypadStateInNmiAndUpdatesTheFrame() throws IOException {
         FrameBufferRenderer renderer =
                 new FrameBufferRenderer(Background.BUFFER_SIZE, Background.BUFFER_SIZE, 60);
@@ -242,6 +261,65 @@ class SyntheticRomBootTest {
         rom[0x7ffc] = 0x00;
         rom[0x7ffd] = (byte) 0x80;
         Path path = tempDir.resolve("synthetic-dma.sfc");
+        Files.write(path, rom);
+        return path;
+    }
+
+    private Path writeHdmaRom() throws IOException {
+        byte[] rom = new byte[0x8000];
+        byte[] program = {
+                (byte) 0x78,                         // SEI
+                (byte) 0xa9, (byte) 0x80,            // LDA #$80
+                (byte) 0x8d, 0x00, 0x21,             // STA $2100 (forced blank)
+                (byte) 0xa9, 0x00,                   // LDA #$00
+                (byte) 0x8d, 0x21, 0x21,             // STA $2121 (CGADD)
+                (byte) 0xa9, 0x1f,                   // LDA #$1f
+                (byte) 0x8d, 0x22, 0x21,             // STA $2122 (CGDATA low)
+                (byte) 0xa9, 0x00,                   // LDA #$00
+                (byte) 0x8d, 0x22, 0x21,             // STA $2122 (CGDATA high)
+                (byte) 0xa9, 0x00,                   // LDA #$00
+                (byte) 0x8d, 0x00, 0x43,             // STA $4300 (one byte)
+                (byte) 0xa9, 0x21,                   // LDA #$21
+                (byte) 0x8d, 0x01, 0x43,             // STA $4301 (CGADD)
+                (byte) 0xa9, 0x00,                   // LDA #$00
+                (byte) 0x8d, 0x02, 0x43,             // STA $4302 (table low)
+                (byte) 0xa9, (byte) 0x81,            // LDA #$81
+                (byte) 0x8d, 0x03, 0x43,             // STA $4303 (table high)
+                (byte) 0xa9, 0x00,                   // LDA #$00
+                (byte) 0x8d, 0x04, 0x43,             // STA $4304 (table bank)
+                (byte) 0xa9, 0x02,                   // LDA #$02
+                (byte) 0x8d, 0x10, 0x43,             // STA $4310 (two bytes to one register)
+                (byte) 0xa9, 0x22,                   // LDA #$22
+                (byte) 0x8d, 0x11, 0x43,             // STA $4311 (CGDATA)
+                (byte) 0xa9, 0x10,                   // LDA #$10
+                (byte) 0x8d, 0x12, 0x43,             // STA $4312 (table low)
+                (byte) 0xa9, (byte) 0x81,            // LDA #$81
+                (byte) 0x8d, 0x13, 0x43,             // STA $4313 (table high)
+                (byte) 0xa9, 0x00,                   // LDA #$00
+                (byte) 0x8d, 0x14, 0x43,             // STA $4314 (table bank)
+                (byte) 0xa9, 0x03,                   // LDA #$03
+                (byte) 0x8d, 0x0c, 0x42,             // STA $420c (enable channels 0 and 1)
+                (byte) 0xa9, 0x0f,                   // LDA #$0f
+                (byte) 0x8d, 0x00, 0x21,             // STA $2100 (display on)
+                (byte) 0x80, (byte) 0xfe             // BRA *
+        };
+        System.arraycopy(program, 0, rom, 0, program.length);
+        rom[0x0100] = 0x01;
+        rom[0x0101] = 0x00;
+        rom[0x0102] = 0x00;
+        rom[0x0110] = 0x01;
+        rom[0x0111] = (byte) 0xe0;
+        rom[0x0112] = 0x03;
+        rom[0x0113] = 0x00;
+        byte[] title = "JAMSNES HDMA PROBE".getBytes(StandardCharsets.ISO_8859_1);
+        System.arraycopy(title, 0, rom, 0x7fc0, title.length);
+        rom[0x7fd5] = 0x20;
+        rom[0x7fd6] = 0x00;
+        rom[0x7fd7] = 0x05;
+        rom[0x7fd8] = 0x00;
+        rom[0x7ffc] = 0x00;
+        rom[0x7ffd] = (byte) 0x80;
+        Path path = tempDir.resolve("synthetic-hdma.sfc");
         Files.write(path, rom);
         return path;
     }
