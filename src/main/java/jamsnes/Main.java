37ed213b915e149fc7969ef7f4dc47083385ae11
ahianf <ahian.fernandez@outlook.com>
2026-08-16 22:48:25 -0400

    add java maven skeleton

---
+++ src/main/java/jamsnes/Main.java
@@ -0,0 +1,10 @@
+package jamsnes;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("JamSNES Java port");
+    }
+}
---
+++ src/test/java/jamsnes/MainTest.java
@@ -0,0 +1,12 @@
+package jamsnes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class MainTest {
+    @Test
+    void mainRuns() {
+        assertDoesNotThrow(() -> Main.main(new String[0]));
+    }
+}
